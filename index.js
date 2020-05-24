ed6fd76ce1b32e4e27ab02ba2faa5212d963cf73
detols <mpsericpark@gmail.com>
2020-05-24 14:07:18 -0400

    Added and created some additional functions for finding articles given a category

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ require('dotenv').config();
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI(process.env.API_KEY); // api key fom .env
 
+// stringContainsCategories - returns a boolean indicating whether string contains all categories
+function stringContainsCategories(string, categories) {
+	let containsCategory = true;
+	
+	categories.forEach((term) => {
+		if (!string.toLowerCase().includes(term)) {
+			containsCategory = false;
+		}
+	});
+	
+	return containsCategory;
+}
+
 // stringContainsCovid - returns a boolean indicating whether a string contains covid
 function stringContainsCovid(string) {
   // example coronavirus terms
@@ -22,6 +35,17 @@ function stringContainsCovid(string) {
   return containsCovid;
 }
 
+// articleContainsCategory - returns a boolean indicating whether the article contains the categories
+function articleContainsCategory(article, categories) {
+	// iterate through each property in the article object for strings
+	Object.keys(article).forEach(propKey => {
+		if (stringContainsCategories(article[propKey] + '', categories)) {
+			return true;
+		}
+	});
+	return false;
+}
+
 // articleContainsCovid - returns a boolean indicating whether the article contains covid
 function articleContainsCovid(article) {
   // iterate through each property in the article object for strings
@@ -33,6 +57,22 @@ function articleContainsCovid(article) {
   return false;
 }
 
+// getCategoryHeadlines - returns an array of all categorized headlines
+async function getCategoryHeadlines(language, country, categories) {
+	//Get headlines
+	const articles = (await newsapi.v2.topHeadlines({language, country})).articles;
+	
+	// reduce to a set of articles to a subset that contains articles
+	const categorizedArticles = articles.reduce((safe, article) => {
+		if (articleContainsCategory(article, categories)) {
+			safe.push(article);
+		}
+		return safe;
+	}, []);
+	
+	return categorizedArticles;
+}
+
 // getCovidFreeHeadlines - returns an array of covid free headlines
 async function getCovidFreeHeadlines(language, country) {
   // get the headlines
@@ -40,7 +80,7 @@ async function getCovidFreeHeadlines(language, country) {
 
   // reduce to a set of articles to a subset without covid
   const covidFree = articles.reduce((safe, article) => {
-    // if the article is covid free, add it to the 'safe' arary
+    // if the article is covid free, add it to the 'safe' array
     if (!articleContainsCovid(article)) {
       safe.push(article);
     }
@@ -52,6 +92,12 @@ async function getCovidFreeHeadlines(language, country) {
 }
 
 // get the articls and print them
+const givenCategories = ['korea']; 
+
 getCovidFreeHeadlines('en', 'us')
+  .then((articles) => console.log(articles))
+  .catch((rej) => console.log(rej));
+
+getCategoryHeadlines('en', 'us', givenCategories)
   .then((articles) => console.log(articles))
   .catch((rej) => console.log(rej));
